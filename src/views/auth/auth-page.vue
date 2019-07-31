b2332ea0f07ee08e12531dc60cca5a13087ddc69
Yoones Khoshghadam <yooneskh@gmail.com>
2019-07-31 12:28:49 +0430

    fixes on auth

--- src/views/auth/auth-page.vue
+++ src/views/auth/auth-page.vue
@@ -1,11 +1,36 @@
 <template>
-  <div class="auth-page">
-    AuthPage
-  </div>
+  <v-container bg fill-height>
+    <v-layout row wrap align-center>
+      <v-flex class="px-4">
+        
+        <v-card max-width="385" class="mx-auto" outlined>
+          <v-card-title class="text-center">Login</v-card-title>
+          <v-card-text class="pt-1 pb-0">
+
+            <v-form>
+              <v-text-field label="Phone Number" />
+            </v-form>
+
+          </v-card-text>
+          <v-card-actions>
+            <v-btn block large color="primary" @click="mode = 'register'">{{mode}}</v-btn>
+          </v-card-actions>
+        </v-card>
+
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'AuthPage'
+  name: 'AuthPage',
+  data: () => ({
+    mode: 'login'
+  })
 };
 </script>
+
+<style lang="scss" scoped>
+
+</style>
