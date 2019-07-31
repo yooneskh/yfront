ff0e2356b066d8aa5b77f4cd810a5789560c394c
Yoones Khoshghadam <yooneskh@gmail.com>
2019-07-31 11:51:45 +0430

    flexible view

--- src/app.vue
+++ src/app.vue
@@ -1,11 +1,25 @@
 <template>
-  <v-app>
-    <router-view/>
+  <v-app style="background: rgba(black, 0.2);">
+    <y-flexible-view style="background: white;">
+      <router-view />
+    </y-flexible-view>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'ApplicationBase'
+  name: 'ApplicationBase',
+  components: {
+    'y-flexible-view': () => import('./components/y-flexible-view')
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+  .app {
+    background: rgba(black, 0.2);
+    .flexible-view {
+      background: white;
+    }
+  }
+</style>
---
+++ src/components/y-flexible-view.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="y-flexible-view" :class="{'compact': isDesktop}">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'YFlexibleView',
+  computed: {
+    isMobile() {
+      return window.innerWidth <= 450
+    },
+    isTablet() {
+      return window.innerWidth > 450 && window.innerWidth <= 850
+    },
+    isDesktop() {
+      return window.innerWidth > 850
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .y-flexible-view {
+    min-height: 100%;
+    &.compact {
+      display: block;
+      width: 690px;
+      margin: auto;
+      box-shadow: 0 0 16px 4px rgba(black, 0.1);
+    }
+  }
+</style>
