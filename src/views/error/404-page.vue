379cee9ed5003bd879b81ddc3ff023756062ae2c
Yoones Khoshghadam <yooneskh@gmail.com>
2019-12-23 13:02:46 +0330

    fix 404 page

--- src/views/error/404-page.vue
+++ src/views/error/404-page.vue
@@ -1,14 +1,14 @@
 <template>
-  <v-container fluid bg fill-height style="background-color: #EAEAEA">
-    <v-layout row wrap align-center>
-      <v-flex class="px-4 text-center">
+  <v-container fluid fill-height style="background-color: #EAEAEA">
+    <v-row align-center class="ma-0">
+      <v-col class="px-4 text-center">
         
         <h1 class="text-error">صفحه مورد نظر شما موجود نیست!</h1>
 
         <v-btn class="mt-6" large color="error" to="/">بریم به صفحه اصلی!</v-btn>
 
-      </v-flex>
-    </v-layout>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
