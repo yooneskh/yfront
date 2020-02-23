1880d49ddb89d10931ee7b1f9567fa1ee5ba5da5
Yoones Khoshghadam <yooneskh@gmail.com>
2020-02-23 15:13:06 +0330

    beter y rich visualizer

--- src/components/y-rich-visualizer.vue
+++ src/components/y-rich-visualizer.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="y-rich-visualizer px-4 py-4">
+  <div class="y-rich-visualizer" :style="`padding: ${paddingVertical}px ${paddingHorizontal}px`">
     <div v-for="(part, index) in parts" :key="part.type + index">
       <div v-if="part.type === 'title'" class="headline font-weight-bold">{{ part.content }}</div>
       <p v-if="part.type === 'text'" class="body-1">{{ part.content }}</p>
-      <v-img v-if="part.type === 'image'" class="mx-n4" :src="part.content" />
+      <v-img v-if="part.type === 'image'" :src="part.content" style="max-width: unset;" :style="`margin: 0 -${paddingHorizontal}px`" />
     </div>
   </div>
 </template>
@@ -12,7 +12,15 @@
 export default {
   name: 'YRichVisualizer',
   props: {
-    text: String
+    text: String,
+    paddingVertical: {
+      type: Number,
+      default: 24
+    },
+    paddingHorizontal: {
+      type: Number,
+      default: 24
+    }
   },
   computed: {
     parts() {
