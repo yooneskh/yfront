12b55ddf04a997107521527b1b81179e620e0074
Yoones Khoshghadam <yooneskh@gmail.com>
2020-12-31 17:10:42 +0330

    fix -base- back color

--- src/views/core/main-base.vue
+++ src/views/core/main-base.vue
@@ -133,7 +133,7 @@ export default {
 
 <style lang="scss" scoped>
   .main-base {
-    background: #EAEAEA;
+    background: #F5F5F5;
     .loading-container {
       position: fixed;
       top: 0; right: 0; bottom: 0; left: 0;
