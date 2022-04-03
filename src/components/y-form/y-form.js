cf7c3caf06124fa942f5bb9e4089269f41b1862b
Yoones Khoshghadam <yooneskh@gmail.com>
2022-04-03 12:16:09 +0430

    feat (form elements): add live html

--- src/components/y-form/y-form.js
+++ src/components/y-form/y-form.js
@@ -8,6 +8,7 @@ export default {
     'y-form-element-permissions': () => import('./y-form-elements/y-form-element-permissions.vue' /* webpackChunkName: 'y-form-element-permissions' */),
     'y-form-element-json': () => import('./y-form-elements/y-form-element-json.vue' /* webpackChunkName: 'y-form-element-json' */),
     'y-form-element-json-schema': () => import('./y-form-elements/y-form-element-json-schema.vue' /* webpackChunkName: 'y-form-element-json-schema' */),
-    'y-form-element-geo': () => import('./y-form-elements/y-form-element-geo.vue' /* webpackChunkName: 'y-form-element-geo' */)
+    'y-form-element-geo': () => import('./y-form-elements/y-form-element-geo.vue' /* webpackChunkName: 'y-form-element-geo' */),
+    'y-form-element-live-html': () => import('./y-form-elements/y-form-element-live-html.vue' /* webpackChunkName: 'y-form-element-live-html' */)
   }
 }
